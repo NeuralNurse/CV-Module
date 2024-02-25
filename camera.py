1195bf93ab2d0565253e0989d5ce60397d11d331
Lumijek <lumijekpr@gmail.com>
2024-02-25 11:11:10 -0800

    first commit

---
+++ camera.py
@@ -0,0 +1,114 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+model = YOLO("../YOLO Weights/yolov8n.pt")
+
+classNames = [
+    "person",
+    "bicycle",
+    "car",
+    "motorbike",
+    "aeroplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "sofa",
+    "pottedplant",
+    "bed",
+    "diningtable",
+    "toilet",
+    "tvmonitor",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+            cv2.rectangle(img, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=1)
+
+            conf = math.ceil((box.conf[0] * 100)) / 100
+
+            object_cls = box.cls[0]
+            name = classNames[int(object_cls)]
+
+            cv2.putText(img, name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, 50)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
