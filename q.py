b8830be1129aebfa4edc35d3fe92dc6d9bd05450
Lumijek <lumijekpr@gmail.com>
2024-02-25 15:41:47 -0800

    added voice recognizable input

--- q.py
+++ q.py
@@ -1,23 +1,43 @@
+import time
 import sys
 import face_recognition
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import threading
+import speech_recognition as sr
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
+in_progress = []
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+def get_name(r, known_face_encodings, known_face_names, face_encoding):
+    try:   
+        with sr.Microphone() as source2:
+            r.adjust_for_ambient_noise(source2, duration = 2)
 
-# Get a reference to webcam #0 (the default one)
+            print("start")             
+            audio2 = r.listen(source2)
+             
+            text = r.recognize_google(audio2)
+            text = text.lower()
+
+            known_face_encodings.append(face_encoding)
+            known_face_names.append(text)
+            in_progress.remove(face_encoding)
+             
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        in_progress.remove(face_encoding)
+
+         
+    except sr.UnknownValueError:
+        print("unknown error occurred")
+        in_progress.remove(face_encoding)
+
+
+r = sr.Recognizer() 
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
 obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
@@ -47,8 +67,6 @@ while True:
     if process_this_frame:
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-
         rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
         
         face_locations = face_recognition.face_locations(rgb_small_frame)
@@ -59,49 +77,37 @@ while True:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
-
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
             else:
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-                cropped_im = frame[left:right, bottom:top]
-                plt.imshow(cropped_im)
-                plt.show()
-                name = input("Enter unknown face name: ")
-                known_face_encodings.append(face_encoding)
-                known_face_names.append(name)
+                if(in_progress != []):
+                    continue
+                in_progress.append(face_encoding)
+                name_thread = threading.Thread(target=get_name, args=(r, known_face_encodings, known_face_names, face_encoding,))
+                name_thread.start()
+
             face_names.append(name)
 
     process_this_frame = not process_this_frame
 
-
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-        # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
     cv2.imshow('Video', frame)
 
-    # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
